86b26167d0f9d8b57cb94f800f3439c5b12b0545
“Ignacio <nachorf78@gmail.com>
2023-04-14 18:02:08 -0300

    DetectorMinasCentroYEsquina

--- src/ar/edu/unlam/dominio/Tablero.java
+++ src/ar/edu/unlam/dominio/Tablero.java
@@ -33,9 +33,7 @@ public Integer columnaRandom() {
 	return columnaAleatoria;	
 }
 
-public void creacionJuego() {
-	Integer filaRandom = filaRandom();
-	Integer columnaRandom = columnaRandom();
+public void creacionJuegoFacil() {
 	
 	for (int i = 0; i < tablero.length; i++) {
 		for (int j = 0; j < tablero.length; j++) {
@@ -45,12 +43,78 @@ public void creacionJuego() {
 		}
 	}
 	
-	this.tablero[filaRandom][columnaRandom] = 0;
+	//Genera Variable
+	Integer filaRandom1 ;
+	Integer columnaRandom1 ;
+	
+	Integer filaRandom2 ;
+	Integer columnaRandom2 ;
+	
+	Integer filaRandom3 ;
+	Integer columnaRandom3 ;
+
+	//Asigna Valor	
+	filaRandom1 = filaRandom();
+	columnaRandom1 = columnaRandom();
+		
+	filaRandom2 = filaRandom();
+	columnaRandom2 = columnaRandom();
+		
+	filaRandom3 = filaRandom();
+	columnaRandom3 = columnaRandom();
+	
+if (filaRandom1 == filaRandom2 && columnaRandom1 == columnaRandom2) {
+		
+		if(columnaRandom2 == 3) {
+			columnaRandom2--;
+		} else {
+			columnaRandom2++;
+			if (filaRandom2 == 3) {
+				filaRandom2--;
+			} else {
+				filaRandom2++;
+			}
+		}
+		
+ }
+if (filaRandom3 == filaRandom2 && columnaRandom3 == columnaRandom2) {
+		
+		if(columnaRandom2 == 3) {
+			columnaRandom2--;
+		} else {
+			columnaRandom2++;
+			if (filaRandom2 == 3) {
+				filaRandom2--;
+			} else {
+				filaRandom2++;
+			}
+		}
+		
+ }
+
+if (filaRandom1 == filaRandom3 && columnaRandom1 == columnaRandom3) {
+	
+	if(columnaRandom3 == 3) {
+		columnaRandom3--;
+	} else {
+		columnaRandom3++;
+		if (filaRandom3 == 3) {
+			filaRandom3--;
+		} else {
+			filaRandom3++;
+		}
+	}
+	
+}
+
+this.tablero[filaRandom1][columnaRandom1] = 0;
+this.tablero[filaRandom2][columnaRandom2] = 0;
+this.tablero[filaRandom3][columnaRandom3] = 0;
 		
 }
 
 public void mostrarTablero() {
-	creacionJuego();
+	creacionJuegoFacil();
 	for (int i = 0; i < tablero.length; i++) {
 		for (int j = 0; j < tablero.length; j++) {
 		System.out.print(this.tablero[i][j] + " ");
@@ -60,5 +124,125 @@ public void mostrarTablero() {
 	}
 
 }
+
+
+
+public void comprobarCantidadBomba(Integer fila,Integer columna) {
+	Integer cantidadBombas = 0;
+	Integer filaReal = fila - 1;
+	Integer columnaReal = columna - 1;
+	
+	if(this.tablero[filaReal][columnaReal] != 0) {
+		
+		bombasMedio(cantidadBombas, filaReal, columnaReal);
+		
+		esquinaSuperiorIzquierda(cantidadBombas, filaReal, columnaReal);
+		
+		esquinaInferiorIzquierda(cantidadBombas, filaReal, columnaReal);
+		
+		esquinaSuperiorDerecha(cantidadBombas, filaReal, columnaReal);
+		
+		esquinaInferiorDerecha(cantidadBombas, filaReal, columnaReal);
+
+		
+		
+	} else {
+		System.out.println("Sos un burro perdiste");
+	}
+	
+	
+}
+
+
+private void esquinaInferiorDerecha(Integer cantidadBombas, Integer filaReal, Integer columnaReal) {
+	if (filaReal == 4 && columnaReal == 4) {
+		if (this.tablero[filaReal-1][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal-1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+	}
+}
+
+
+private void esquinaSuperiorDerecha(Integer cantidadBombas, Integer filaReal, Integer columnaReal) {
+	if (filaReal == 0 && columnaReal == 4) {
+		if (this.tablero[filaReal-1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+	}
+}
+
+
+private void esquinaInferiorIzquierda(Integer cantidadBombas, Integer filaReal, Integer columnaReal) {
+	if (filaReal == 4 && columnaReal == 0) {
+		if (this.tablero[filaReal-1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal-1][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+	}
+}
+
+
+private void esquinaSuperiorIzquierda(Integer cantidadBombas, Integer filaReal, Integer columnaReal) {
+	if (filaReal == 0 && columnaReal == 0) {
+		if (this.tablero[filaReal][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+	}
+}
+
+
+private void bombasMedio(Integer cantidadBombas, Integer filaReal, Integer columnaReal) {
+	if(filaReal != 0 && columnaReal != 0 && filaReal != 3 && columnaReal != 3 ) {
+		if (this.tablero[filaReal-1][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal-1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal-1][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal-1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal+1][columnaReal] == 0) {
+			cantidadBombas++;
+		}
+		if (this.tablero[filaReal][columnaReal+1] == 0) {
+			cantidadBombas++;
+		}
+	}
+}
+
+
 
 }
--- src/ar/edu/unlam/interfaz/Juego.java
+++ src/ar/edu/unlam/interfaz/Juego.java
@@ -1,14 +1,86 @@
 package ar.edu.unlam.interfaz;
 
+import java.util.Scanner;
+
 import ar.edu.unlam.dominio.Tablero;
 
 public class Juego {
+	private static final int DIFICULTAD = 1;
+	private static final int PEQUENIO = 1;
+	private static final int MEDIANO = 2;
+	private static final int GRANDE = 3;
+	static Scanner teclado = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Tablero tablero = new Tablero();
-		
+
+		/*
+		 * hola dificultad seleccione casilla fila/columna
+		 * 
+		 */
+		mostrarMensaje("Bienvenido al buscaminas");
+		int opcion;
+		do {
+			mostrarMensaje("Elija la opcion que desee\n1.Para seleccionar la dificultad");
+			opcion = teclado.nextInt();
+			switch (opcion) {
+			case DIFICULTAD:
+				eleccionDificultad(tablero);
+				break;
+
+			default:
+				mostrarMensaje("Opcion incorrecta ");
+				break;
+			}
+		} while (opcion != 10);
+
 		tablero.mostrarTablero();
 	}
 
-}
+	private static void mostrarMensaje(String mensaje) {
+		System.out.println(mensaje);
+
+	}
+
+	private static void eleccionDificultad(Tablero tablero) {
+		int opcion;
+		do {
+			mostrarMensaje("1. Facil\n2. Intermedio \n3. Técnico Especialista en Desactivación de Artefactos Explosivos ");
+			opcion=teclado.nextInt();
+		switch (opcion) {
+		case PEQUENIO:
+         tablero.creacionJuegoFacil();
+         tablero.mostrarTablero();
+         
+			comienzoJuego(tablero);
+         
+         
+         
+         
+			break;
+
+		case MEDIANO:
+//			 tablero.creacionJuegoMedio();
+			break;
+
+		case GRANDE:
+//			 tablero.creacionJuegoTécnicoEspecialistaEnDesactivaciónDeArtefactosExplosivos();
+			break;
+		
+		default:
+			mostrarMensaje("Opcion incorrecta");
+			break;
+		}	
+		} while (opcion != 10);
+		}
+
+	private static void comienzoJuego(Tablero tablero) {
+		mostrarMensaje("Seleccione una fila");
+         Integer fila = teclado.nextInt();
+         mostrarMensaje("Seleccionar columna");
+         Integer columna = teclado.nextInt();
+         tablero.comprobarCantidadBomba(fila, columna);
+	}
+		
+	}
